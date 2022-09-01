bbfbece2cbf03569bfe0f131efb3e77741948594
ramma.fitra <rammafitracyt@gmail.com>
2022-09-01 16:10:41 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React from 'react';
+
+
+
+function App() {
+ 
+ 
+  return (
+    
+    <div className='App'>
+       <div class="grid-container">
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 1</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 2</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 3</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 4</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 5</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 6</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 7</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 8</h3>
+        </div>
+        <div class="grid-item"><img src={'/stream'} className="App-logo" alt="logo"/>
+        <h3>Stream 9</h3>
+        </div>
+      </div> 
+    </div>
+    
+   
+  );
+}
+export default App;
